26c6396f4dd5a5b381a27a6422c858f4f20b7979
TorVik247 <99217595+TorVik247@users.noreply.github.com>
2023-11-15 22:54:56 +0200

    0.2

--- src/main/java/com/example/demo/HelloApplication.java
+++ src/main/java/com/example/demo/HelloApplication.java
@@ -7,12 +7,16 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
+
+
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        stage.setMinHeight(420);
+        stage.setMinWidth(365);
         Scene scene = new Scene(fxmlLoader.load(), 350, 380);
-        stage.setTitle("Hello!");
+        stage.setTitle("Golova");
         stage.setScene(scene);
         stage.show();
     }
