07edc3f91cc4fb72ee1a1f33ee9c1b2be0c335a3
TorVik247 <99217595+TorVik247@users.noreply.github.com>
2023-11-16 13:29:02 +0200

    0.3

--- src/main/java/com/example/demo/HelloApplication.java
+++ src/main/java/com/example/demo/HelloApplication.java
@@ -1,8 +1,12 @@
 package com.example.demo;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -20,6 +24,8 @@ public class HelloApplication extends Application {
         stage.setScene(scene);
         stage.show();
     }
+
+
 
     public static void main(String[] args) {
         launch();
--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,14 +1,50 @@
 package com.example.demo;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private Button btnAdd;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    void showDialog(ActionEvent event) {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloController.class.getResource("edit.fxml"));
+
+        try {
+            Stage stage = new Stage();
+            Scene scene = new Scene(fxmlLoader.load(),400,150);
+            stage.setScene(scene);
+
+            stage.setTitle("Редагування");
+            stage.setMinHeight(170);
+            stage.setMinWidth(400);
+            stage.initModality(Modality.WINDOW_MODAL);
+            stage.initOwner(btnAdd.getScene().getWindow());
+            stage.show();
+
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    void new_Alert(ActionEvent event){
+        Alert alert = new Alert(Alert.AlertType.WARNING);
+        alert.setTitle("Видалення");
+
+        alert.setHeaderText("Results:");
+            alert.setContentText("Видалення може призвести до зміни порядку осіб у Адресній книзі! ");
+
+        alert.showAndWait();
     }
 }
